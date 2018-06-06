e920b6bad81c3ebd5ac819ffd179c92aadbcb720
HUNG-PC\HUNG <trantienhung77@yahoo.com>
2018-06-05 20:55:46 -0500

    - Convert the ChooseNextWayPoint to C++

--- Source/Apocalypse/Private/MyChooseNextWayPoint.cpp
+++ Source/Apocalypse/Private/MyChooseNextWayPoint.cpp
@@ -1,10 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MyChooseNextWayPoint.h"
+#include "AIController.h"
+#include "Public/MyTP_ThirdPersonCharacter.h" // TODO remove coupling PatrollingGuard
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UMyChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Execute C++ task report for duty!"));
+	// Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<AMyTP_ThirdPersonCharacter>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	// set next patrol point
+	auto BlackboardComponent = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComponent->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComponent->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComponent->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+	// TODO protect against empty patrol routes
 	return EBTNodeResult::Succeeded;
 }
 
--- Source/Apocalypse/Public/MyChooseNextWayPoint.h
+++ Source/Apocalypse/Public/MyChooseNextWayPoint.h
@@ -15,4 +15,10 @@ class APOCALYPSE_API UMyChooseNextWayPoint : public UBTTaskNode
 	GENERATED_BODY()
 
 		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector WaypointKey;
 };
