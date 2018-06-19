c5a6c0c47639ba3121c75ce90a69ca8d9077c829
HUNG-PC\HUNG <trantienhung77@yahoo.com>
2018-06-18 21:49:10 -0500

    - Move Patrol Route to C++ and deleted empty thirdperson character files

--- Source/Apocalypse/Private/MyChooseNextWayPoint.cpp
+++ Source/Apocalypse/Private/MyChooseNextWayPoint.cpp
@@ -2,16 +2,24 @@
 
 #include "MyChooseNextWayPoint.h"
 #include "AIController.h"
-#include "Public/MyTP_ThirdPersonCharacter.h" // TODO remove coupling PatrollingGuard
+#include "Public/PatrolRoute.h" // TODO remove coupling PatrollingGuard
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UMyChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+
 	// Get the patrol points
 	auto AIController = OwnerComp.GetAIOwner();
 	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<AMyTP_ThirdPersonCharacter>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if(!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	// Warn against empty route point
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A Guard has no patrol points"));
+	}
 
 	// set next patrol point
 	auto BlackboardComponent = OwnerComp.GetBlackboardComponent();
