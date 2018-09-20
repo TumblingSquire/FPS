b94c9837d4e79f339a3a5be5972b5692ce44c132
HUNG-PC\HUNG <trantienhung77@yahoo.com>
2018-09-19 20:46:16 -0500

    - Reconfigure Gun to fire and spawn projectile at the right place  - Added gun fire sound and animation

--- Source/TestingGrounds/Player/FirstPersonCharacter.cpp
+++ Source/TestingGrounds/Player/FirstPersonCharacter.cpp
@@ -52,6 +52,10 @@ void AFirstPersonCharacter::BeginPlay()
 	// TODO Attach gun to character mesh component
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+
+	// TODO Bind OnFire() from the Gun class
+	// Bind fire event
+	InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -66,9 +70,6 @@ void AFirstPersonCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	// TODO Bind OnFire() from the Gun class
-	// Bind fire event
-	// PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFirstPersonCharacter::OnFire);
 
 	// Enable touchscreen input
 	EnableTouchscreenMovement(PlayerInputComponent);
--- Source/TestingGrounds/Weapons/Gun.h
+++ Source/TestingGrounds/Weapons/Gun.h
@@ -44,10 +44,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+
+public:
 	/** Fires a projectile. */
 	void OnFire();
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
